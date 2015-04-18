51a80e066f8aec16d23d3b080bf046a1d2b43054
mgi166 <skskoari@gmail.com>
2015-04-18 19:09:04 +0900

    Imprement the minimal Module#extend * To minimize the change of object

--- lib/enchant_encoder/file_encoder/encode_proxy.rb
+++ lib/enchant_encoder/file_encoder/encode_proxy.rb
@@ -6,16 +6,27 @@ module FileEncoder
 
     def initialize(obj)
       @obj = obj.dup
-
-      if EXTEND_METHOS.any? { |method| @obj.respond_to?(method) }
-        @obj.extend(FileEncoder::Nkfable)
-      else
-        @obj
-      end
+      extend_methods!
     end
 
     def method_missing(method, *args, &block)
       @obj.send(method, *args, &block)
+    end
+
+    def extend_methods!
+      EXTEND_METHOS.each do |method|
+        if @obj.respond_to?(method)
+          const_name = "Nkf#{method.capitalize}"
+
+          begin
+            module_name = FileEncoder::Proxy.const_get(const_name)
+            @obj.extend(module_name)
+          rescue NameError
+          end
+        end
+      end
+
+      @obj
     end
   end
 end
--- spec/lib/enchant_encoder/encode_proxy_spec.rb
+++ spec/lib/enchant_encoder/encode_proxy_spec.rb
@@ -13,10 +13,26 @@ RSpec.describe FileEncoder::EncodeProxy do
             end.to yield_successive_args("あああ\n", "いいい\n", "ううう\n")
           end
         end
+
+        context '#foreach' do
+          it do
+            expect do
+              proxy.foreach
+            end.to raise_error NoMethodError
+          end
+        end
       end
 
       context 'class' do
         let(:file) { File }
+
+        context '#each' do
+          it do
+            expect do
+              proxy.each
+            end.to raise_error NoMethodError
+          end
+        end
 
         context '#foreach' do
           it "yields converted string by NKF" do
