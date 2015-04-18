656ff2dcc586693487818005c28e69ed93fc431e
mgi166 <skskoari@gmail.com>
2015-04-18 18:44:24 +0900

    Check whether extend method is already defined

--- lib/enchant_encoder/file_encoder/encode_proxy.rb
+++ lib/enchant_encoder/file_encoder/encode_proxy.rb
@@ -2,9 +2,16 @@ require 'nkf'
 
 module FileEncoder
   class EncodeProxy
+    EXTEND_METHOS = %w(each foreach)
+
     def initialize(obj)
       @obj = obj.dup
-      @obj.extend(FileEncoder::Proxy::Nkfable)
+
+      if EXTEND_METHOS.any? { |method| @obj.respond_to?(method) }
+        @obj.extend(FileEncoder::Nkfable)
+      else
+        @obj
+      end
     end
 
     def method_missing(method, *args, &block)
