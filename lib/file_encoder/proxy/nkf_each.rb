5de4e35eae20dc4c5ae099001f1fa3e9f375e47e
mgi166 <skskoari@gmail.com>
2015-04-18 18:56:15 +0900

    Add FileEncoder::Proxy::NkfEach * move Nkfable#each to FileEncoder::Proxy::NkfEach#each

--- lib/file_encoder/proxy/nkf_each.rb
+++ lib/file_encoder/proxy/nkf_each.rb
@@ -1,6 +1,6 @@
 module FileEncoder::Proxy
-  module Nkfable
-    def foreach(*args, &block)
+  module NkfEach
+    def each(*args, &block)
       super(*args) do |row|
         yield ::NKF.nkf('-Lu -w -m0', row)
       end
