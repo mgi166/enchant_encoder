6bb3b88477faf7bb17f079ceff810dab1dffa5cf
mgi166 <skskoari@gmail.com>
2015-04-18 23:24:08 +0900

    Remove MethodProxy * merge to EncodeProxy

--- lib/enchant_encoder/encode_proxy.rb
+++ lib/enchant_encoder/encode_proxy.rb
@@ -1,3 +1,4 @@
+require 'file_encoder/nkf_each.rb'
 require 'nkf'
 
 module FileEncoder
@@ -6,8 +7,45 @@ module FileEncoder
       @obj = obj
     end
 
+    def each(*args, &block)
+      if @obj.respond_to?(:each)
+        if block_given?
+          @obj.each do |row|
+            yield ::NKF.nkf('-Lu -w -m0', row)
+          end
+        else
+          @obj.each.extend(NkfEach)
+        end
+      else
+        @obj.each(*args, &block)
+      end
+    end
+
+    def foreach(*args, &block)
+      if @obj.respond_to?(:foreach)
+        if block_given?
+          @obj.foreach(*args) do |row|
+            yield ::NKF.nkf('-Lu -w -m0', row)
+          end
+        else
+          @obj.foreach(*args).extend(NkfEach)
+        end
+      else
+        @obj.foreach(*args, &block)
+      end
+    end
+
+    def open(*args, &block)
+      if @obj.respond_to?(:open)
+        io = @obj.open(*args).extend(NkfEach)
+        block_given? ? yield(io) : io
+      else
+        @obj.open(*args, &block)
+      end
+    end
+
     def method_missing(method, *args, &block)
-      MethodProxy.new(@obj).send(method, *args, &block)
+      @obj.send(method, *args, &block)
     end
   end
 end
