8ce3de9799c6cd506347f336b99988771bef7aa5
mgi166 <skskoari@gmail.com>
2015-04-19 10:25:41 +0900

    Remove spec of CSV instance

--- spec/lib/file_encoder/encode_proxy/csv_spec.rb
+++ spec/lib/file_encoder/encode_proxy/csv_spec.rb
@@ -4,113 +4,67 @@ RSpec.describe FileEncoder::EncodeProxy do
   describe '#method_missing' do
     context 'when CSV' do
       let(:proxy) { FileEncoder::EncodeProxy.new(csv) }
+      let(:csv) { CSV }
 
-      context 'instance' do
-        let(:csv) { CSV.open(src_path('data-simple-sjis.txt')) }
+      context '#each' do
+        it do
+          expect do
+            proxy.each
+          end.to raise_error NoMethodError
+        end
+      end
 
-        context '#each' do
-          context 'block given' do
-            it "yields converted string by NKF" do
-              expect do |b|
-                proxy.each(&b)
-              end.to yield_successive_args("あああ\n", "いいい\n", "ううう\n")
-            end
-          end
-
-          context 'no block given' do
-            it do
-              expect(proxy.each).to be_instance_of Enumerator
-            end
-
-            it "yields converted string by NKF" do
-              enum = proxy.each
-              expect do |b|
-                enum.each(&b)
-              end.to yield_successive_args("あああ\n", "いいい\n", "ううう\n")
-            end
+      context '#foreach' do
+        context 'block given' do
+          it "yields converted string by NKF" do
+            expect do |b|
+              proxy.foreach(src_path('data-simple-sjis.txt'), &b)
+            end.to yield_successive_args("あああ\n", "いいい\n", "ううう\n")
           end
         end
 
-        context '#foreach' do
+        context 'no block given' do
           it do
-            expect do
-              proxy.foreach
-            end.to raise_error NoMethodError
+            expect(proxy.foreach(src_path('data-simple-sjis.txt'))).to be_instance_of Enumerator
           end
-        end
 
-        context '#open' do
-          it do
-            expect do
-              proxy.open
-            end.to raise_error NoMethodError
+          it "yields converted string by NKF" do
+            enum = proxy.foreach(src_path('data-simple-sjis.txt'))
+            expect do |b|
+              enum.each(&b)
+            end.to yield_successive_args("あああ\n", "いいい\n", "ううう\n")
           end
         end
       end
 
-      context 'class' do
-        let(:csv) { CSV }
+      context '#open' do
+        context 'block given' do
+          it "yields converted string by NKF" do
+            expect do |b|
+              proxy.open(src_path('data-simple-sjis.csv')) do |f|
+                f.each(&b)
+              end
+            end.to yield_successive_args(
+              ["あああ", "いいい", "ううう"],
+              ["えええ", "おおお", "かかか"],
+            )
+          end
+        end
 
-        context '#each' do
+        context 'no block given' do
           it do
-            expect do
-              proxy.each
-            end.to raise_error NoMethodError
-          end
-        end
-
-        context '#foreach' do
-          context 'block given' do
-            it "yields converted string by NKF" do
-              expect do |b|
-                proxy.foreach(src_path('data-simple-sjis.txt'), &b)
-              end.to yield_successive_args("あああ\n", "いいい\n", "ううう\n")
-            end
+            csv = proxy.open(src_path('data-simple-sjis.csv'))
+            expect(csv).to be_instance_of CSV
           end
 
-          context 'no block given' do
-            it do
-              expect(proxy.foreach(src_path('data-simple-sjis.txt'))).to be_instance_of Enumerator
-            end
-
-            it "yields converted string by NKF" do
-              enum = proxy.foreach(src_path('data-simple-sjis.txt'))
-              expect do |b|
-                enum.each(&b)
-              end.to yield_successive_args("あああ\n", "いいい\n", "ううう\n")
-            end
-          end
-        end
-
-        context '#open' do
-          context 'block given' do
-            it "yields converted string by NKF" do
-              expect do |b|
-                proxy.open(src_path('data-simple-sjis.csv')) do |f|
-                  f.each(&b)
-                end
-              end.to yield_successive_args(
-                ["あああ", "いいい", "ううう"],
-                ["えええ", "おおお", "かかか"],
-              )
-            end
-          end
-
-          context 'no block given' do
-            it do
-              csv = proxy.open(src_path('data-simple-sjis.csv'))
-              expect(csv).to be_instance_of CSV
-            end
-
-            it "yields converted string by NKF" do
-              csv = proxy.open(src_path('data-simple-sjis.csv'))
-              expect do |b|
-                csv.each(&b)
-              end.to yield_successive_args(
-                ["あああ", "いいい", "ううう"],
-                ["えええ", "おおお", "かかか"],
-              )
-            end
+          it "yields converted string by NKF" do
+            csv = proxy.open(src_path('data-simple-sjis.csv'))
+            expect do |b|
+              csv.each(&b)
+            end.to yield_successive_args(
+              ["あああ", "いいい", "ううう"],
+              ["えええ", "おおお", "かかか"],
+            )
           end
         end
       end
