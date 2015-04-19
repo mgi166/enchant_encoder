75d89e8d2b41deffd7121679d7cfd40179271177
mgi166 <skskoari@gmail.com>
2015-04-19 15:49:22 +0900

    Add FileEncorder::CsvRefiner

--- lib/enchant_encoder/encode_proxy.rb
+++ lib/enchant_encoder/encode_proxy.rb
@@ -2,48 +2,8 @@ require 'file_encoder/nkf_each.rb'
 require 'csv'
 require 'nkf'
 
-module CSVExtention
-  refine CSV.singleton_class do
-    def open(*args)
-      # find the +options+ Hash
-      options = if args.last.is_a? Hash then args.pop else Hash.new end
-      # wrap a File opened with the remaining +args+ with no newline
-      # decorator
-      file_opts = {universal_newline: false}.merge(options)
-      begin
-        # NOTE: This IO object that I want to extend module
-        f = File.open(*args, file_opts).extend(FileEncoder::NkfGets)
-      rescue ArgumentError => e
-        raise unless /needs binmode/ =~ e.message and args.size == 1
-        args << "rb"
-        file_opts = {encoding: Encoding.default_external}.merge(file_opts)
-        retry
-      end
-      begin
-        csv = new(f, options)
-      rescue Exception
-        f.close
-        raise
-      end
-
-      # handle blocks like Ruby's open(), not like the CSV library
-      if block_given?
-        begin
-          yield csv
-        ensure
-          csv.close
-        end
-      else
-        csv
-      end
-    end
-  end
-end
-
 module FileEncoder
   class EncodeProxy
-    using CSVExtention
-
     def initialize(obj)
       @obj = obj
     end
---
+++ lib/file_encoder/csv_refiner.rb
@@ -0,0 +1,49 @@
+module FileEncoder
+  module CSVExtention
+    refine CSV.singleton_class do
+      def open(*args)
+        # find the +options+ Hash
+        options = if args.last.is_a? Hash then args.pop else Hash.new end
+        # wrap a File opened with the remaining +args+ with no newline
+        # decorator
+        file_opts = {universal_newline: false}.merge(options)
+        begin
+          # NOTE: This IO object that I want to extend module
+          f = File.open(*args, file_opts).extend(FileEncoder::NkfGets)
+        rescue ArgumentError => e
+          raise unless /needs binmode/ =~ e.message and args.size == 1
+          args << "rb"
+          file_opts = {encoding: Encoding.default_external}.merge(file_opts)
+          retry
+        end
+        begin
+          csv = new(f, options)
+        rescue Exception
+          f.close
+          raise
+        end
+
+        # handle blocks like Ruby's open(), not like the CSV library
+        if block_given?
+          begin
+            yield csv
+          ensure
+            csv.close
+          end
+        else
+          csv
+        end
+      end
+
+      def foreach(path, options = Hash.new, &block)
+        return to_enum(__method__, path, options) unless block
+        open(path, options) do |csv|
+          csv.each(&block)
+        end
+      end
+    end
+  end
+
+  class CsvRefiner
+  end
+end
